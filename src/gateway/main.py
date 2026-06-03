839db65cbfbb3789406c64962492b6ef2b6a7e24
Matias Valmaggia <mvalmaggia@fi.uba.ar>
2026-06-03 20:44:15 -0300

    modifying gateway message protocol

--- src/gateway/main.py
+++ src/gateway/main.py
@@ -25,7 +25,6 @@ from src.common.communication.internal import (
     serialize,
 )
 
-
 CONFIG_PATH = "./config.yaml"
 
 
@@ -63,12 +62,15 @@ def init_config():
             "ACCOUNTS_QUEUE",
             file_config.get("accounts_queue", "accounts_queue"),
         ),
-        result_queue=os.getenv("INPUT_QUEUE", file_config.get("result_queue", "result_queue")),
+        result_queue=os.getenv(
+            "RESULT_QUEUE",
+            file_config.get("result_queue", "result_queue"),
+        ),
         log_level=os.getenv("LOG_LEVEL", file_config.get("log_level", "INFO")),
     )
 
 
-def log_config(config):
+def log_config(config: GatewayConfig):
     logging.info(
         "Gateway startup with: host=%s | port=%s | mom_host=%s | transactions_queue=%s | accounts_queue=%s | result_queue=%s",
         config.host,
@@ -128,182 +130,215 @@ def _dict_to_account_row(row):
     return AccountRow(**kwargs)
 
 
-class GatewayService:
-    def __init__(self, config):
-        self.host = config.host
-        self.port = config.port
-        self.mom_host = config.mom_host
-        self.transactions_queue = config.transactions_queue
-        self.accounts_queue = config.accounts_queue
-        self.result_queue = config.result_queue
-        self._tx_mw = None
-        self._accounts_mw = None
-        self._result_mw = None
-        self._server_sock = None
-        self._running = False
+class Gateway:
+    def __init__(self, gateway_config: GatewayConfig):
+        self.server_host = gateway_config.host
+        self.server_port = gateway_config.port
+        self.mom_host = gateway_config.mom_host
+        self.transactions_queue_name = gateway_config.transactions_queue
+        self.accounts_queue_name = gateway_config.accounts_queue
+        self.result_queue_name = gateway_config.result_queue
+
+        self.transactions_mw = None
+        self.accounts_mw = None
+        self.result_mw = None
+
+        self.server_socket = None
+        self.running = False
+
         self._send_lock = threading.Lock()
         self._registry_lock = threading.Lock()
-        self._registry: dict = {}
+        self._client_registry = {}
+        self._client_threads = []
         self._result_thread = None
-        self._client_threads: list = []
 
-    def start(self):
-        logging.info("Starting gateway service")
-        self._running = True
-        self._tx_mw = MessageMiddlewareQueueRabbitMQ(self.mom_host, self.transactions_queue)
-        self._accounts_mw = MessageMiddlewareQueueRabbitMQ(self.mom_host, self.accounts_queue)
-        self._result_mw = MessageMiddlewareQueueRabbitMQ(self.mom_host, self.result_queue)
-
-        self._result_thread = threading.Thread(
-            target=self._result_mw.start_consuming,
-            args=(self._dispatch_result,),
-            daemon=True,
+    def _setup_middleware(self):
+        self.transactions_mw = MessageMiddlewareQueueRabbitMQ(
+            self.mom_host, self.transactions_queue_name
         )
-        self._result_thread.start()
-
-        self._server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self._server_sock.bind((self.host, self.port))
-        self._server_sock.listen(8)
-        logging.info("Gateway listening on %s:%s", self.host, self.port)
-
-        try:
-            while self._running:
-                try:
-                    client_sock, addr = self._server_sock.accept()
-                except OSError:
-                    break
-                logging.info("Client connected from %s", addr)
-                t = threading.Thread(
-                    target=self._serve_one_client,
-                    args=(client_sock,),
-                    daemon=True,
-                )
-                t.start()
-                self._client_threads.append(t)
-        finally:
-            self._close_resources()
+        self.accounts_mw = MessageMiddlewareQueueRabbitMQ(
+            self.mom_host, self.accounts_queue_name
+        )
+        self.result_mw = MessageMiddlewareQueueRabbitMQ(
+            self.mom_host, self.result_queue_name
+        )
 
     def _dispatch_result(self, message, ack, _nack):
-        try:
-            decoded = deserialize(message)
-        except Exception:
-            logging.exception("error decoding result message; discarding")
+            try:
+                decoded = deserialize(message)
+            except Exception:
+                logging.exception("Error decoding result message; discarding")
+                ack()
+                return
+
+            client_id = decoded.get("client")
+
+            with self._registry_lock:
+                entry = self._client_registry.get(client_id)
+
+            if entry is None:
+                logging.warning("No client registered for id %s; discarding result", client_id)
+                ack()
+                return
+
+            try:
+                entry["tcp"].send_bytes(message)
+            except Exception:
+                logging.exception("Error forwarding result to client %s", client_id)
+                entry["done"].set()
+                ack()
+                return
+
             ack()
-            return
 
-        client_id = decoded.get("client")
-        with self._registry_lock:
-            entry = self._registry.get(client_id)
+            if decoded.get("type") == "eof":
+                logging.info("Received EOF on result queue for client %s", client_id)
+                entry["done"].set()
 
-        if entry is None:
-            logging.warning("No client registered for id %s; discarding result", client_id)
-            ack()
-            return
-
-        try:
-            entry["tcp"].send_bytes(message)
-        except Exception:
-            logging.exception("error forwarding result to client %s", client_id)
-            entry["done"].set()
-            ack()
-            return
-
-        ack()
-        if decoded.get("type") == "eof":
-            logging.info("Received EOF on result queue for client %s", client_id)
-            entry["done"].set()
-
-    def _serve_one_client(self, sock):
-        tcp = TCPSocket(sock)
+    def handle_client_request(self, client_socket, msg_handler):
+        tcp = TCPSocket(client_socket)
         client_id = str(uuid.uuid4())
         done = threading.Event()
 
         with self._registry_lock:
-            self._registry[client_id] = {"tcp": tcp, "done": done}
+                self._client_registry[client_id] = {
+                    "tcp": tcp,
+                    "done": done,
+                }
+
         logging.info("Registered client %s", client_id)
 
         try:
-            for stream, batch in receive_streams(tcp):
-                if not batch:
-                    continue
-                if stream == STREAM_TRANSACTIONS:
-                    txs = [_dict_to_transaction_row(row) for row in batch]
-                    msg = build_raw_transactions_message(
+            while True:
+                try:
+                    raw_bytes = client_socket.recv_bytes()
+                except ConnectionError:
+                    logging.info("The client has disconnected")
+                    break
+                
+                message = communication.internal.deserialize(raw_bytes)
+                msg_type = message.get("type")
+
+                if msg_type == "raw_transactions":
+                    txs = [_dict_to_transaction_row(row) for row in message.get("batch", [])]
+                    serialized_message = communication.internal.serialize(build_raw_transactions_message(
                         client=client_id,
                         msg_id=str(uuid.uuid4()),
                         batch=txs,
-                    )
+                    ))
+
                     with self._send_lock:
-                        self._tx_mw.send(serialize(msg))
-                elif stream == STREAM_ACCOUNTS:
-                    accounts = [_dict_to_account_row(row) for row in batch]
-                    msg = build_raw_accounts_message(
+                        self.transactions_queue.send(serialized_message)
+
+                elif msg_type == "raw_accounts":
+                    accounts = [_dict_to_account_row(row) for row in message.get("batch", [])]
+                    serialized_message = communication.internal.serialize(build_raw_accounts_message(
                         client=client_id,
                         msg_id=str(uuid.uuid4()),
                         batch=accounts,
-                    )
+                    ))
                     with self._send_lock:
-                        self._accounts_mw.send(serialize(msg))
-                else:
-                    logging.warning("Unknown stream byte: %d", stream)
+                        self.accounts_queue.send(serialized_message)
+
             logging.info("Inbound EOF received for client %s; forwarding EOF downstream", client_id)
+            eof_message = serialize(
+                build_eof_message(client=client_id, msg_id=str(uuid.uuid4()))
+            )
+
             with self._send_lock:
-                self._tx_mw.send(serialize(build_eof_message(client=client_id, msg_id=str(uuid.uuid4()))))
-                self._accounts_mw.send(serialize(build_eof_message(client=client_id, msg_id=str(uuid.uuid4()))))
+                self.transactions_mw.send(eof_message)
+                self.accounts_mw.send(eof_message)
+
             done.wait()
             logging.info("Pipeline complete for client %s; closing client socket", client_id)
-        except ConnectionError:
-            logging.info("Client %s disconnected during inbound", client_id)
+                
+        except socket.error:
+            logging.error("The connection with the server was lost")
+        except Exception as e:
+            logging.error(e)
         finally:
             with self._registry_lock:
-                self._registry.pop(client_id, None)
-            tcp.close()
+                self._client_registry.pop(client_id, None)
+            client_socket.close()
 
-    def stop(self):
-        logging.info("Stopping gateway service")
-        self._running = False
-        if self._server_sock is not None:
-            try:
-                self._server_sock.close()
-            except Exception:
-                logging.exception("error closing server socket")
-        if self._result_mw is not None:
-            try:
-                self._result_mw.stop_consuming()
-            except Exception:
-                logging.exception("error stopping result consumer")
+    def handle_client_response(self):
+            self.result_mw.start_consuming(self._dispatch_result)
 
-    def _close_resources(self):
-        if self._server_sock is not None:
+    @staticmethod
+    def handle_sigterm(server_socket, client_list, sigterm_received, signum, frame):
+        """Static method because it doesn't need 'self' state, it only acts on passed arguments."""
+        try:
+            server_socket.shutdown(socket.SHUT_RDWR)
+        except OSError:
+            pass # Ignore if socket is already closed
+
+        for [_, client_socket] in client_list:
             try:
-                self._server_sock.close()
-            except Exception:
+                client_socket.shutdown(socket.SHUT_RDWR)
+            except OSError:
                 pass
-        for mw in (self._tx_mw, self._accounts_mw, self._result_mw):
-            if mw is None:
-                continue
-            try:
-                mw.close()
-            except Exception:
-                logging.exception("error closing middleware")
+        sigterm_received.value = 1
 
+    def run(self):
+        logging.info("Starting Gateway...")
+        self.running = True
+
+        self._setup_middleware()
+
+        self._result_thread = threading.Thread(target=self._handle_client_response, deaemon=True)
+        self._result_thread.start()
+
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.bind((self.server_host, self.server_port))
+        self.server_socket.listen(8)
+
+        logging.info(
+            "Listening for connections on %s:%s",
+            self.server_host,
+            self.server_port,
+        )
+
+        try:
+            while self.running:
+                try:
+                    client_socket_socket, addr = self.server_socket.accept()
+                except OSError:
+                    break
+
+                logging.info("A new client has connected from %s", addr)
+
+                client_thread = threading.Thread(
+                    target=self.handle_client_request,
+                    args=(client_socket_socket, message_handler.MessageHandler()),
+                    daemon=True,
+                )
+                client_thread.start()
+                self._client_threads.append(client_thread)
+        except Exception as e:
+            logging.error(f"Unexpected error in accept loop: {e}")
+        finally:
+            self._close_resources()
+            
+        return 0
 
 def main():
     config = init_config()
-    logging.basicConfig(level=getattr(logging, config.log_level.upper(), logging.INFO))
+    logging.basicConfig(
+        level=getattr(logging, config.log_level.upper(), logging.INFO)
+    )
     log_config(config)
-    service = GatewayService(config)
+
+    gateway = Gateway(config)
 
     def handle_sigterm(signum, frame):
-        logging.info("Received SIGTERM signal")
-        service.stop()
+        logging.info("Received shutdown signal %s", signum)
+        gateway.stop()
 
     signal.signal(signal.SIGTERM, handle_sigterm)
     signal.signal(signal.SIGINT, handle_sigterm)
-    service.start()
-    return 0
 
+    return gateway.run()
 
 if __name__ == "__main__":
     raise SystemExit(main())
